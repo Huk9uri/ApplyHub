5f0c3607e29ea4b800f2aa1a9c3a47716fbf62e4
huk9uri <huk9uri@gmail.com>
2026-06-17 22:41:14 +0900

    chore: Spring Boot 백엔드 기본 설정 추가

---
+++ backend/src/main/java/com/applyhub/ApplyHubApplication.java
@@ -0,0 +1,12 @@
+package com.applyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyHubApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/applyhub/ApplyHubApplicationTests.java
@@ -0,0 +1,14 @@
+package com.applyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
